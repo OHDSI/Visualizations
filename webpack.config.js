6a9572555c4839df3613a7d19af0570dd33fe123
Pavel Grafkin <pavgra@gmail.com>
2017-09-26 02:17:57 +0300

    Added assembling of UMD module with webpack (#11)
    * added webpack to build process to produce umd library

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const webpack = require('webpack');
+
+const config = {
+  entry: {
+    main: './atlascharts/main.js'
+  },
+  output: {
+    filename: 'dist/atlascharts.umd.js',
+    path: __dirname,
+    libraryTarget: 'umd'
+  },
+  externals: {
+    d3: 'd3',
+    numeral: 'numeral',
+    get: 'get'
+  },
+  module: {
+    loaders: [{
+      test: /\.js?$/,
+      exclude: /node_modules/,
+      loaders: ['babel-loader'],
+    }]
+  },
+  plugins: [],
+};
+
+config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+  comments: false,
+}));
+config.plugins.push(new webpack.optimize.OccurrenceOrderPlugin());
+
+module.exports = config;
